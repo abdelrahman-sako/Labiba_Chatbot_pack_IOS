bb51d1c3ee891289f673a89b9cf77971ce7685f0
Abdulrahman <amalhaririy@gmail.com>
2021-09-01 10:42:32 +0300

    fix some issues

---
+++ LabibaBotFramwork/Framework/Classes/Theme and Settings/HumanAgentSettings.swift
@@ -0,0 +1,9 @@
+//
+//  HumanAgentSettings.swift
+//  LabibaBotFramwork
+//
+//  Created by Abdulrahman Qasem on 12/08/2021.
+//  Copyright © 2021 Abdul Rahman. All rights reserved.
+//
+
+import Foundation
