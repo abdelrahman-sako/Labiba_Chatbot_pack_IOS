7fd144d5d000d4d7d3404491ba9d7de3c0af6977
Abdulrahman <amalhaririy@gmail.com>
2021-10-28 10:11:06 +0300

    add update token request with its settings

---
+++ LabibaBotFramwork/Framework/Classes/Models/UpdateTokenModel.swift
@@ -0,0 +1,9 @@
+//
+//  UpdateTokenModel.swift
+//  LabibaBotFramwork
+//
+//  Created by Abdulrahman Qasem on 14/10/2021.
+//  Copyright © 2021 Abdul Rahman. All rights reserved.
+//
+
+import Foundation
