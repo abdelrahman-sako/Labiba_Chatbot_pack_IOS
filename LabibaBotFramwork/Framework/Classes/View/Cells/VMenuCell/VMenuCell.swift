227702ffd4ea396590283866fcf0061eec04b117
Abdulrahman Qasem <32479304+amalhaririy@users.noreply.github.com>
2023-06-07 10:06:15 +0300

    no image in vmenu is handled

--- LabibaBotFramwork/Framework/Classes/View/Cells/VMenuCell/VMenuCell.swift
+++ LabibaBotFramwork/Framework/Classes/View/Cells/VMenuCell/VMenuCell.swift
@@ -9,52 +9,60 @@
 import UIKit
 
 class VMenuCell: UITableViewCell {
-
+  
+  @IBOutlet weak var trailingStackViewConst: NSLayoutConstraint!
+  @IBOutlet weak var leadingStackViewConst: NSLayoutConstraint!
   @IBOutlet weak var stackView: UIStackView!
   @IBOutlet weak var actionButton: UIButton!
-    @IBOutlet weak var contanierView: UIView!
-    @IBOutlet weak var menuContentLabel: UILabel!
-    @IBOutlet weak var menuImageView: UIImageView!
+  @IBOutlet weak var contanierView: UIView!
+  @IBOutlet weak var menuContentLabel: UILabel!
+  @IBOutlet weak var menuImageView: UIImageView!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    setupUI()
+  }
+  
+  override func setSelected(_ selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        setupUI()
+  }
+  
+  func setupUI(){
+    menuImageView.isHidden = Labiba.vMenuTheme.isImageHidden
+    menuImageView.backgroundColor = Labiba.vMenuTheme.imageBackgroundColor
+    menuImageView.tintColor  = Labiba.vMenuTheme.imageTintColor
+    
+    menuImageView.roundCorners(corners: Labiba.vMenuTheme.imageCornerTheme.corners,
+                               radius: Labiba.vMenuTheme.imageCornerTheme.radius)
+    
+    menuContentLabel.font = Labiba.vMenuTheme.contentLabelTheme.fontStyle
+    menuContentLabel.textColor = Labiba.vMenuTheme.contentLabelTheme.textColor
+    
+    contanierView.layer.cornerRadius = 8
+    
+    stackView.semanticContentAttribute = Labiba.botLang == .en ? .forceLeftToRight : .forceRightToLeft
+    menuContentLabel.semanticContentAttribute  = Labiba.botLang == .en ? .forceLeftToRight : .forceRightToLeft
+    if let shadow = Labiba.vMenuTheme.shadow {
+      contanierView.applyShadow(color: shadow.color,
+                                opacity: shadow.opacity, offset: shadow.offset,radius: shadow.radius)
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
+  }
+  
+  
+  func setData(model:DialogCard){
+    menuContentLabel.text = model.title
+    if let url = URL(string: model.imageUrl ?? ""){
+      menuImageView.af_setImage(withURL: url)
+      leadingStackViewConst.constant = 5
+      trailingStackViewConst.constant = 5
+    } else {
+      menuImageView.isHidden = true
+      leadingStackViewConst.constant = 20
+      trailingStackViewConst.constant = 20
     }
     
-    func setupUI(){
-        menuImageView.isHidden = Labiba.vMenuTheme.isImageHidden
-        menuImageView.backgroundColor = Labiba.vMenuTheme.imageBackgroundColor
-        menuImageView.tintColor  = Labiba.vMenuTheme.imageTintColor
-        
-        menuImageView.roundCorners(corners: Labiba.vMenuTheme.imageCornerTheme.corners,
-                                   radius: Labiba.vMenuTheme.imageCornerTheme.radius)
-        
-        menuContentLabel.font = Labiba.vMenuTheme.contentLabelTheme.fontStyle
-        menuContentLabel.textColor = Labiba.vMenuTheme.contentLabelTheme.textColor
-        
-        contanierView.layer.cornerRadius = 8
-        
-      stackView.semanticContentAttribute = Labiba.botLang == .en ? .forceLeftToRight : .forceRightToLeft
-      menuContentLabel.semanticContentAttribute  = Labiba.botLang == .en ? .forceLeftToRight : .forceRightToLeft
-        if let shadow = Labiba.vMenuTheme.shadow {
-            contanierView.applyShadow(color: shadow.color,
-                                      opacity: shadow.opacity, offset: shadow.offset,radius: shadow.radius)
-        }        
-    }
     
-
-    func setData(model:DialogCard){
-        guard let url = URL(string: model.imageUrl ?? "") else {
-            return
-        }
-        menuImageView.af_setImage(withURL: url)
-        menuContentLabel.text = model.title
-        
-    }
-    
+  }
+  
 }
