87cc39032db0f281bd20a73013ad88ae57b5d7e9
Osama Hasan <>
2024-09-18 09:26:19 +0300

    Fixes pdf sent

--- LabibaBotFramwork/Framework/Classes/BubbleView.swift
+++ LabibaBotFramwork/Framework/Classes/BubbleView.swift
@@ -14,16 +14,16 @@ enum BubbleSource
     case outgoing
 }
 
-  public class BubbleView: UIView {
+public class BubbleView: UIView {
     
     class func createBubble(withWidth width:CGFloat) -> BubbleView { return BubbleView()}
     
     var considersAvatar:Bool = true
     
-      @IBOutlet weak var bubbleHeightConstraint: NSLayoutConstraint!
-      @IBOutlet weak var timeStackview: UIStackView!
-      @IBOutlet weak var timeImageView: UIImageView!
-      @IBOutlet weak var textLabel: KGCopyableLabel!
+    @IBOutlet weak var bubbleHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var timeStackview: UIStackView!
+    @IBOutlet weak var timeImageView: UIImageView!
+    @IBOutlet weak var textLabel: KGCopyableLabel!
     @IBOutlet weak var bubbleContainer: UIView!
     @IBOutlet weak var timestampLbl: UILabel!
     public override  func accessibilityElementDidBecomeFocused() {
@@ -32,7 +32,7 @@ enum BubbleSource
         }
         TextToSpeechManeger.Shared.downloadFileFromURL(url:url )
     }
-   
+    
     
     var posY:CGFloat = 0
     var maxWidth:CGFloat = 0
@@ -44,12 +44,12 @@ enum BubbleSource
             if Labiba.hasBubbleTimestamp {
                 if let timestamp = currentDialog?.timestamp {
                     let dateFormatter = DateFormatter()
-                   
+                    
                     dateFormatter.dateFormat = source == .incoming ? Labiba.BotChatBubble.timestamp.formate : Labiba.UserChatBubble.timestamp.formate
                     let botName = Labiba.BotChatBubble.botName == nil ? "bot".localForChosnLangCodeBB : Labiba.BotChatBubble.botName!
                     let userName = Labiba.UserChatBubble.userName == nil ? "you".localForChosnLangCodeBB : Labiba.UserChatBubble.userName!
                     
-                  
+                    
                     if SharedPreference.shared.botLangCode == .ar{
                         dateFormatter.locale = Locale(identifier: "ar")
                         timestampLbl.text = "\(source == .incoming ? botName : userName) - \(dateFormatter.string(from: timestamp))"
@@ -63,23 +63,23 @@ enum BubbleSource
                     }
                     
                     timestampLbl.isHidden  = false
-//                    if botName == "bot".localForChosnLangCodeBB {
-                        timeImageView?.isHidden = false
-//                    }
+                    //                    if botName == "bot".localForChosnLangCodeBB {
+                    timeImageView?.isHidden = false
+                    //                    }
                     timeStackview?.isHidden = false
                     
                 }else {
                     timestampLbl.isHidden  = true
                     timeImageView?.isHidden = true
                     timeStackview?.isHidden = true
-
+                    
                 }
             }else{
                 timestampLbl.isHidden  = true
                 timeImageView?.isHidden = true
                 timeStackview?.isHidden = true
-
-               
+                
+                
             }
             if let frame = currentDialog?.frame {
                 self.frame = frame
@@ -88,15 +88,15 @@ enum BubbleSource
                 self.isAccessibilityElement = true
                 self.accessibilityLabel = "رسالة منك \(currentDialog?.attributedMessage?.string) "
                 addLinkGesture()
-               
+                
             }else
             {
-            checkHyperLink(text:newValue)
-            var text = newValue.trimmingCharacters(in: .whitespacesAndNewlines)
-            print("TrackSteps Bubble " + text)
-            print("TrackSteps ########################################")
-            /////////////////////////////////////html
-            
+                checkHyperLink(text:newValue)
+                var text = newValue.trimmingCharacters(in: .whitespacesAndNewlines)
+                print("TrackSteps Bubble " + text)
+                print("TrackSteps ########################################")
+                /////////////////////////////////////html
+                
                 self._message = text
                 self.isAccessibilityElement = true
                 self.accessibilityLabel = "رسالة منك \(_message) "
@@ -111,12 +111,13 @@ enum BubbleSource
                 }
                 else
                 {
+                    let formattedLinestext = text.replacingOccurrences(of: "\n", with: "<br>", options: .literal, range: nil)
                     fontSize = Labiba.BotChatBubble.fontsize
                     let lang = text.detectedLangauge()
                     if lang == "ar" {text.addArabicAlignment()}
                     let boldFont =  applyBotFont(textLang: LabibaLanguage(rawValue: lang ?? "") ?? .ar ,bold:true, size: fontSize )
                     let regularFont =  applyBotFont(textLang: LabibaLanguage(rawValue: lang ?? "") ?? .ar , size: fontSize)
-                    self.textLabel.attributedText = text.htmlAttributedString(regularFont:regularFont, boldFont: boldFont ,color: Labiba.BotChatBubble.textColor)
+                    self.textLabel.attributedText = formattedLinestext.htmlAttributedString(regularFont:regularFont, boldFont: boldFont ,color: Labiba.BotChatBubble.textColor)
                 }
                 
                 let lang = textLabel.text?.detectedLangauge()
@@ -157,12 +158,12 @@ enum BubbleSource
         }
         currentDialog?.link = url
         addLinkGesture()
-     
+        
     }
     func addLinkGesture()   {
         guard let _ = currentDialog?.link else {
-                   return
-               }
+            return
+        }
         let gesture = UITapGestureRecognizer(target: self, action: #selector(bubbleDidTap))
         gesture.numberOfTapsRequired = 1
         self.isUserInteractionEnabled = true
@@ -189,19 +190,19 @@ enum BubbleSource
         
         let TextXPosition = self.textLabel.frame.origin.x + 1.0
         let TextPadding = CGFloat(TextXPosition * 2.0) // Text padding
-
+        
         let BubbleXPosition:CGFloat
         let avatarWidth = self.considersAvatar ? AvatarWidth : 0.0
         let avatarMargin:CGFloat = self.considersAvatar ? 5 : 0.0
         
         let totalMargin = avatarMargin + avatarWidth + ipadMargin + margin
-
+        
         
         let maxWidth =  self.maxWidth - _bubbleMargin - TextPadding  - ipadFactor*(ipadMargin + 70)
         let size = text?.size(maxWidth: maxWidth   , font: applyBotFont(textLang:lang, size: fontSize)) ?? CGSize(width: 50, height: 50)
         var height = size.height
         
-         height += (20 + ipadFactor*6)
+        height += (20 + ipadFactor*6)
         let width = size.width < 55.0 ? 55.0 : size.width
         
         if self.source == .incoming
@@ -224,7 +225,7 @@ enum BubbleSource
     
     func popUp() -> Void
     {
-       // self.alpha = 0
+        // self.alpha = 0
         self.transform = CGAffineTransform(scaleX: 0.4, y: 0.4)
         UIView.animate(withDuration: 0.2)
         {
@@ -236,90 +237,90 @@ enum BubbleSource
     
     
     
-  
+    
     
 }
 
 
 
 class KGCopyableLabel: UILabel {
-//    override init(frame: CGRect) {
-//           super.init(frame: frame)
-//           self.sharedInit()
-//       }
-//
-//       required init?(coder aDecoder: NSCoder) {
-//           super.init(coder: aDecoder)
-//           self.sharedInit()
-//       }
-//
-//       func sharedInit() {
-//           self.isUserInteractionEnabled = true
-//           let gesture = UILongPressGestureRecognizer(target: self, action: #selector(self.showMenu))
-//           self.addGestureRecognizer(gesture)
-//       }
-//
-//       @objc func showMenu(_ recognizer: UILongPressGestureRecognizer) {
-//           self.becomeFirstResponder()
-//
-//           let menu = UIMenuController.shared
-//
-//           let locationOfTouchInLabel = recognizer.location(in: self)
-//
-//           if !menu.isMenuVisible {
-//               var rect = bounds
-//               rect.origin = locationOfTouchInLabel
-//               rect.size = CGSize(width: 1, height: 1)
-//
-//            if #available(iOS 13.0, *) {
-//                menu.showMenu(from: self, rect: rect)
-//            } else {
-//                // Fallback on earlier versions
-//            }
-//           }
-//       }
-//
-//       override func copy(_ sender: Any?) {
-//           let board = UIPasteboard.general
-//
-//           board.string = text
-//
-//           let menu = UIMenuController.shared
-//
-//           menu.setMenuVisible(false, animated: true)
-//       }
-//
-//       override var canBecomeFirstResponder: Bool {
-//           return true
-//       }
-//
-//       override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
-//           return action == #selector(UIResponderStandardEditActions.copy)
-//       }
+    //    override init(frame: CGRect) {
+    //           super.init(frame: frame)
+    //           self.sharedInit()
+    //       }
+    //
+    //       required init?(coder aDecoder: NSCoder) {
+    //           super.init(coder: aDecoder)
+    //           self.sharedInit()
+    //       }
+    //
+    //       func sharedInit() {
+    //           self.isUserInteractionEnabled = true
+    //           let gesture = UILongPressGestureRecognizer(target: self, action: #selector(self.showMenu))
+    //           self.addGestureRecognizer(gesture)
+    //       }
+    //
+    //       @objc func showMenu(_ recognizer: UILongPressGestureRecognizer) {
+    //           self.becomeFirstResponder()
+    //
+    //           let menu = UIMenuController.shared
+    //
+    //           let locationOfTouchInLabel = recognizer.location(in: self)
+    //
+    //           if !menu.isMenuVisible {
+    //               var rect = bounds
+    //               rect.origin = locationOfTouchInLabel
+    //               rect.size = CGSize(width: 1, height: 1)
+    //
+    //            if #available(iOS 13.0, *) {
+    //                menu.showMenu(from: self, rect: rect)
+    //            } else {
+    //                // Fallback on earlier versions
+    //            }
+    //           }
+    //       }
+    //
+    //       override func copy(_ sender: Any?) {
+    //           let board = UIPasteboard.general
+    //
+    //           board.string = text
+    //
+    //           let menu = UIMenuController.shared
+    //
+    //           menu.setMenuVisible(false, animated: true)
+    //       }
+    //
+    //       override var canBecomeFirstResponder: Bool {
+    //           return true
+    //       }
+    //
+    //       override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
+    //           return action == #selector(UIResponderStandardEditActions.copy)
+    //       }
     override public var canBecomeFirstResponder: Bool {
         get {
             return true
         }
     }
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setup()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
-
+    
     func setup() {
         isUserInteractionEnabled = true
         let gesture = UITapGestureRecognizer(target: self, action: #selector(showCopyMenu(sender:)))
         gesture.numberOfTapsRequired = 2
-                                            
+        
         addGestureRecognizer(gesture)
     }
-
+    
     override func copy(_ sender: Any?) {
         UIPasteboard.general.string = text
         if #available(iOS 13.0, *) {
@@ -328,7 +329,7 @@ class KGCopyableLabel: UILabel {
             // Fallback on earlier versions
         }
     }
-
+    
     @objc func showCopyMenu(sender: Any?) {
         becomeFirstResponder()
         let menu = UIMenuController.shared
@@ -340,7 +341,7 @@ class KGCopyableLabel: UILabel {
             }
         }
     }
-
+    
     override func canPerformAction(_ action: Selector, withSender sender: Any?) -> Bool {
         return (action == #selector(copy(_:)))
     }
