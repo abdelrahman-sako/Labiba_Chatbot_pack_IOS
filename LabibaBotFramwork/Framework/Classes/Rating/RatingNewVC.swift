53789b70b95b25e229920e2844f176447e9bfc9e
mohammaD95-imagine <m.khalil@labiba.ai>
2025-04-24 09:00:29 +0300

    numberRating changes

---
+++ LabibaBotFramwork/Framework/Classes/Rating/RatingNewVC.swift
@@ -0,0 +1,30 @@
+//
+//  RatingNewVC.swift
+//  LabibaBotFramwork
+//
+//  Created by Mohammad Khalil on 10/04/2025.
+//  Copyright © 2025 Abdul Rahman. All rights reserved.
+//
+
+import UIKit
+
+class RatingNewVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
