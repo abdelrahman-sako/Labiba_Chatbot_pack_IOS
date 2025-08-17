7d92d9606020eb2af10c6d2a4f7ff107e6a2512a
mohammaD95-imagine <m.khalil@labiba.ai>
2025-08-17 15:08:55 +0300

    test

--- LabibaBotFramwork/Framework/Classes/BotBubble.swift
+++ LabibaBotFramwork/Framework/Classes/BotBubble.swift
@@ -12,7 +12,7 @@ let BotBubbleColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
 
 public class BotBubble: BubbleView {
     
-   
+    // Method
     override class func createBubble(withWidth width:CGFloat) -> BubbleView {
        
         let bubble = UIView.loadFromNibNamedFromDefaultBundle("BotBubble") as! BotBubble
