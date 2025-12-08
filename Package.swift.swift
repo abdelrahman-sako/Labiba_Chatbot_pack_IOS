b6f9e00713270e641abd925ea33b43d2406d8860
mohammaD95-imagine <m.khalil@labiba.ai>
2025-12-08 16:16:27 +0300

    Initial commit for SPM

---
+++ Package.swift.swift
@@ -0,0 +1,28 @@
+//
+//  Package.swift.swift
+//  LabibaBotFramework
+//
+//  Created by Mohammad Khalil on 08/12/2025.
+//  Copyright © 2025 Abdul Rahman. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "LabibaBotFramework",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "LabibaBotFramework",
+            targets: ["LabibaBotFramework"]
+        )
+    ],
+    targets: [
+        .target(
+            name: "MySDK",
+            path: "Sources"
+        )
+    ]
+)
